c1adbc7d3080616ab980beed43079c69f600ea93
eedalong <ee_dalong@163.com>
2021-06-10 14:43:51 +0800

    add mp1_implementatiton

--- mp1_implement.cu
+++ mp1_implement.cu
@@ -1,79 +1,86 @@
-#include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
+// MP 1
+#include	<wb.h>
 
+#define BLOCK_SIZE 256
 
 int ceil(int a, int b){
-    return (a + b - 1) / b;
+    return int((a + b - 1) / b);
 }
-__global__ void vector_add(float* in1, float* in2, float* out, int total_len){
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if(i < total_len){
-        out[i] = in2[i] + in1[i];
+__global__ void vecAdd(float * in1, float * in2, float * out, int len) {
+    //@@ Insert code to implement vector addition here
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if(idx < length){
+        out[idx] = in1[idx] + in2[idx];
     }
 }
 
-bool check_correctness(float* in1, float* in2, float* out, int N){
-    int index = 0;
-    for(int index = 0; index < N; index++){
-        if(out[index] != (in1[index]+in2[index])){
-            return false;
-        }
-    }
-    return true;
+int main(int argc, char ** argv) {
+    wbArg_t args;
+    int inputLength;
+    float * hostInput1;
+    float * hostInput2;
+    float * hostOutput;
+    float * deviceInput1;
+    float * deviceInput2;
+    float * deviceOutput;
+
+    args = wbArg_read(argc, argv);
+
+    wbTime_start(Generic, "Importing data and creating memory on host");
+    hostInput1 = (float *) wbImport(wbArg_getInputFile(args, 0), &inputLength);
+    hostInput2 = (float *) wbImport(wbArg_getInputFile(args, 1), &inputLength);
+    hostOutput = (float *) malloc(inputLength * sizeof(float));
+    wbTime_stop(Generic, "Importing data and creating memory on host");
+
+    wbLog(TRACE, "The input length is ", inputLength);
+
+	wbTime_start(GPU, "Allocating GPU memory.");
+    //@@ Allocate GPU memory here
+    cudaMalloc((void**) &device_input1, sizeof(float) * inputLength);
+    cudaMalloc((void**) &device_input2, sizeof(float) * inputLength);
+    cudaMalloc((void**) &device_output, sizeof(float) * inputLength);
+
+    wbTime_stop(GPU, "Allocating GPU memory.");
+
+    wbTime_start(GPU, "Copying input memory to the GPU.");
+    //@@ Copy memory to the GPU here
+    cudaMemcpy(device_input1, hostInput1, sizeof(float) * length, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_input2, hostInput2, sizeof(float) * length, cudaMemcpyHostToDevice);
+
+
+    wbTime_stop(GPU, "Copying input memory to the GPU.");
     
-}
+    //@@ Initialize the grid and block dimensions here
+    dim3 DimGrid(ceil(length / BLOCK_SIZE), 1, 1);
+    dim3 DimBlock(BLOCK_SIZE, 1, 1);
+    
+    wbTime_start(Compute, "Performing CUDA computation");
+    //@@ Launch the GPU Kernel here
+    vecAdd<<<DimGrid, DimBlock>>>(device_input1, device_input2, device_output);
 
-int main(){
-    srand(time(NULL));
-    // make sure input length is larger than 512 but less than 65535
-    int N = (rand() % (65536-512)) + 512;
-    float* in1;
-    float* in2;
-    float* out;
-    float* device_input1;
-    float* device_input2;
-    float* device_output;
+    cudaThreadSynchronize();
+    wbTime_stop(Compute, "Performing CUDA computation");
+    
+    wbTime_start(Copy, "Copying output memory to the CPU");
+    //@@ Copy the GPU memory back to the CPU here
+    cudaMemcpy(hostOutput, device_output, sizeof(float) * length, cudaMemcpyDeviceToHost);
 
-    int index = 0;
-    // allocate memory for vector
-    in1 = (float*)malloc(N * sizeof(float));
-    in2 = (float*)malloc(N * sizeof(float));
-    out = (float*)malloc(N * sizeof(float));
-    // init vector
-    for(index = 0; index < N; index++){
-        in1[index] = rand() * 1.0;
-        in2[index] = rand() * 1.0;
-    }
-    // allocate device memory 
-    cudaMalloc((void**)&device_input1, N * sizeof(float));
-    cudaMalloc((void**)&device_input2, N * sizeof(float));
-    cudaMalloc((void**)&device_output, N * sizeof(float));
+    wbTime_stop(Copy, "Copying output memory to the CPU");
 
-    // copy vector from host to device
-    cudaMemcpy(device_input1, in1, N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(device_input2, in2, N * sizeof(float), cudaMemcpyHostToDevice);
-
-    dim3 DimGrid(ceil(N, 256), 1, 1);
-    dim3 DimBlock(256, 1, 1);
-    vector_add<<<DimGrid, DimBlock>>>(device_input1, device_input2, device_output, N);
-
-    cudaDeviceSynchronize();
-    cudaMemcpy(out, device_output, N * sizeof(float), cudaMemcpyDeviceToHost);
-    if(check_correctness(in1, in2, out, N)){
-        printf("test passed!");
-    }else{
-        printf("test failed");
-    }
-    // free device memory
+    wbTime_start(GPU, "Freeing GPU Memory");
+    //@@ Free the GPU memory here
     cudaFree(device_input1);
     cudaFree(device_input2);
     cudaFree(device_output);
 
-    // free host memory
-    free(in1);
-    free(in2);
-    free(out);
+    wbTime_stop(GPU, "Freeing GPU Memory");
+
+    wbSolution(args, hostOutput, inputLength);
+
+    free(hostInput1);
+    free(hostInput2);
+    free(hostOutput);
 
     return 0;
 }
+
