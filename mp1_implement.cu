23611576a445b7e5aa8830ff84a1a2d177cacbb6
eedalong <ee_dalong@163.com>
2021-06-10 14:47:34 +0800

    fix typo

--- mp1_implement.cu
+++ mp1_implement.cu
@@ -9,7 +9,7 @@ int ceil(int a, int b){
 __global__ void vecAdd(float * in1, float * in2, float * out, int len) {
     //@@ Insert code to implement vector addition here
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if(idx < length){
+    if(idx < len){
         out[idx] = in1[idx] + in2[idx];
     }
 }
