f7029f73637671c39d0a38964bd332fd223056d6
eedalong <ee_dalong@163.com>
2021-06-11 15:34:07 +0800

    update code

--- mp3_implement.cu
+++ mp3_implement.cu
@@ -30,29 +30,29 @@ __global__ void matrixMultiplyShared(float * A, float * B, float * C,
     int tx = threadIdx.x; int ty = threadIdx.y;
     int Row = by * blockDim.y + ty;
     int Col = bx * blockDim.x + tx;
-    if(Row < numCRows && Col < numCColumns){
-        int step = (numAColumns + BLOCK_SIZE - 1) / BLOCK_SIZE;
-        float PValue = 0;
-        for(int m = 0; m < step; m++){
-            // 1. Load Matrix A Tile
-            if((m * BLOCK_SIZE + tx) >= numAColumns){
-                subTileA[ty][tx] = 0.0;
-            }else{
-                subTileA[ty][tx] = A[Row * numAColumns + m * BLOCK_SIZE + tx];
-            }
-            // 2. Load Matrix B Tile
-            if((m * BLOCK_SIZE + ty) >= numBRows){
-                subTileB[ty][tx] = 0.0;
-            }else{
-                subTileB[ty][tx] = B[(m * BLOCK_SIZE + ty) * numBColumns + Col];
-            }
-            __syncthreads();
-            // 3. Calculate Multiplication
-            for(int k = 0; k < BLOCK_SIZE; k++){
-                PValue += subTileA[ty][k] * subTileB[k][tx];
-            }
-            __syncthreads();
+    int step = (numAColumns + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    float PValue = 0;
+    for(int m = 0; m < step; m++){
+        // 1. Load Matrix A Tile
+        if((m * BLOCK_SIZE + tx) >= numAColumns){
+            subTileA[ty][tx] = 0.0;
+        }else{
+            subTileA[ty][tx] = A[Row * numAColumns + m * BLOCK_SIZE + tx];
         }
+        // 2. Load Matrix B Tile
+        if((m * BLOCK_SIZE + ty) >= numBRows){
+            subTileB[ty][tx] = 0.0;
+        }else{
+            subTileB[ty][tx] = B[(m * BLOCK_SIZE + ty) * numBColumns + Col];
+        }
+        __syncthreads();
+        // 3. Calculate Multiplication
+        for(int k = 0; k < BLOCK_SIZE; k++){
+            PValue += subTileA[ty][k] * subTileB[k][tx];
+        }
+        __syncthreads();
+    }
+    if(Row < numCRows && Col < numCColumns){
         C[Row * numCColumns + Col] = PValue;
     }
 
