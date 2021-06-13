7cef7012b5785767949b1fc6fef9b36baf591895
eedalong <ee_dalong@163.com>
2021-06-13 09:30:42 +0800

    add bank conflict

--- tools/bank_conflict.cu
+++ tools/bank_conflict.cu
@@ -1,11 +1,21 @@
 #include <cuda.h>
 #include <iostream>
 
+/*
+1. stride = 0: all threads request same value, this is where broadcasting happened
+2. stride = 1: all threads request different bank
+3. stride = 2: 2-way bank conflict
+4. stride = 4: 4-way bank conflict
+5. stride = 16: 16-way bank conflict
+6. stride = 32: 32-way bank conflict
+
+*/
 __global__ void TestKernel(unsigned long long* time){
     __shared__ float shared_data[1024];
     unsigned long long startTime = clock();
+    int stride = 2;
     int tid = threadIdx.x;
-    shared_data[tid * 2] ++ ;
+    shared_data[tid * stride] ++ ;
     unsigned long long endTime = clock();
     time = endTime - startTime;
 }
