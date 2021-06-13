943bdf83338e35ae5a438d73f6ae2c6ae56b7eb8
eedalong <ee_dalong@163.com>
2021-06-13 09:42:47 +0800

    update

--- tools/bank_conflict.cu
+++ tools/bank_conflict.cu
@@ -17,7 +17,7 @@ __global__ void TestKernel(unsigned long long* time){
     int tid = threadIdx.x;
     shared_data[tid * stride] ++ ;
     unsigned long long endTime = clock();
-    time = endTime - startTime;
+    *time = endTime - startTime;
 }
 
 
@@ -26,7 +26,7 @@ int main(){
     unsigned long long * dtime;
     cudaMalloc((void**) &dtime, sizeof(unsigned long long));
     for(int index=0; index < 10; index++){
-        TestKernel<<1, 32>>>(dtime);
+        TestKernel<<<1, 32>>>(dtime);
         cudaMemcpy(&time, dtime, sizeof(unsigned long long), cudaMemcpyDeviceToHost);
         std::cout <<"Time: "<<(time - 14) / 32 << std::endl;
         std::cout << std::endl;
