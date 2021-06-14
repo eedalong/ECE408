c512001f20cf8b6bc205a64ce09786f661bd6d1f
eedalong <ee_dalong@163.com>
2021-06-14 13:24:23 +0800

    fix

--- lib/wbSolution.cpp
+++ lib/wbSolution.cpp
@@ -71,8 +71,9 @@ static wbBool wbSolution_correctQ(char * expectedOutputFile, wbSolution_t sol) {
                     idx = ii * expectedColumns + jj;
                     if (wbUnequalQ(expectedData[idx], solutionData[idx])) {
                         string str;
+                        printf("check a: %f \t and b: %f", expectedData[idx], solutionData[idx]);
                         if (expectedColumns == 1) {
-
+                            
                             str = wbString("The solution did not match the expected results at row ", ii,
                                            ". Expecting ", expectedData[idx], " but got ",
                                            solutionData[idx], ".");
