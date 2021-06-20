239e8aecc6b01779cd7a3c657361af5804b3a828
eedalong <ee_dalong@163.com>
2021-06-20 12:42:31 +0800

    add test code

--- tools/test_code.py
+++ tools/test_code.py
@@ -44,7 +44,8 @@ def readImage(dir_path = "../build")->torch.Tensor:
     shape = shape.decode()
     shape = shape.split()
     print(shape[0], shape[1], "")
-    shape = [1, 3] + [int(item) for item in shape]
+    channel_num = 1
+    shape = [1, channel_num] + [int(item) for item in shape]
     print("shape is ", shape)
     total_size = 1
     for dim in shape:
@@ -61,7 +62,7 @@ def readImage(dir_path = "../build")->torch.Tensor:
     assert len(data) == total_size, f"data length {len(data)} not equal to total size {total_size})"
     for row in range(shape[2]):
         for col in range(shape[3]):
-            for channel in range(3):
+            for channel in range(channel_num):
                 image[0][channel][row][col] = int(data[(row * shape[3] + col) * 3 + channel]) / 255.0
     
     return torch.from_numpy(image)
