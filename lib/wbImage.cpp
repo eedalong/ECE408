20e763adc376f072b23bf7fff738e914dcdef4a5
eedalong <ee_dalong@163.com>
2021-06-20 21:04:43 +0800

    da

--- lib/wbImage.cpp
+++ lib/wbImage.cpp
@@ -120,7 +120,7 @@ wbBool wbImage_sameQ(wbImage_t a, wbImage_t b,
           }
           //std::cout<<"( "<<ii<<", "<<jj<<", "<<kk<<" ) = "<<x<<" vs "<<y<<std::endl;
           if (wbUnequalQ(x, y)) {
-            std::cout<<"unequal_1:\t"<<"( "<<ii<<", "<<jj<<", "<<kk<<" ) = "<<x<<" vs "<<y<<std::endl;
+            //std::cout<<"unequal_1:\t"<<"( "<<ii<<", "<<jj<<", "<<kk<<" ) = "<<x<<" vs "<<y<<std::endl;
             
 
             if (onUnSame != NULL) {
