dad380d86257aef0699cccc6832cf9a24c8652ca
eedalong <ee_dalong@163.com>
2021-06-22 21:44:00 +0800

    update

--- mp7_implement.cu
+++ mp7_implement.cu
@@ -212,7 +212,7 @@ int main(int argc, char ** argv) {
     std::cout<<"check input "<<std::endl;
     for(int row = 0; row < 5; row ++){
         for(int col = 0; col < 5; col ++){
-            std::cout<<hostInputImageDataGray[(row * imageWidth + col)]<<", ";
+            std::cout<<(int)hostInputImageDataGray[(row * imageWidth + col)]<<", ";
         }
         std::cout<<endl;
     }
