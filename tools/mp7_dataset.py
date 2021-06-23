2bc1248d8b5274c2a2477a4b2e10b7053c2af0b5
eedalong <ee_dalong@163.com>
2021-06-23 10:49:41 +0800

    update

--- tools/mp7_dataset.py
+++ tools/mp7_dataset.py
@@ -23,7 +23,7 @@ def readPPM(file_path):
     shape = shape.split()
     shape.reverse()
     print(shape[0], shape[1], "")
-    channel_num = 1
+    channel_num = 3
     shape = [channel_num] + [int(item) for item in shape]
     print("shape is ", shape)
     total_size = 1
@@ -39,8 +39,8 @@ def readPPM(file_path):
     print(f"check data[0:10]:\t{data[:10]}")
     image = np.zeros(shape)
     #assert len(data) == total_size, f"data length {len(data)} not equal to total size {total_size})"
-    for row in range(shape[2]):
-        for col in range(shape[3]):
+    for row in range(shape[1]):
+        for col in range(shape[2]):
             for channel in range(channel_num):
                 image[channel][row][col] = int(data[(row * shape[3] + col) * 3 + channel]) / 255.0
     #print(f"check image {image[0][0][:5][:5]}")
@@ -52,6 +52,8 @@ inputImage = 255.0 * inputImage
 inputImage = inputImage.astype(np.ubyte)
 gray_image = inputImage[0] * 0.21 + inputImage[1] * 0.71 + inputImage[2] * 0.07
 gray_image = gray_image.astype(np.ubyte)
+print(gray_image)
+
 globalHist = [0] * 256
 globalCDF = [0] * 256
 for row in range(gray_image.shape[0]):
